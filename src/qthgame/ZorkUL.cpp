9d47149ba9e21753da60c5794b63b1d5aba12c6d
Guilherme Noya <gui.noya@gmail.com>
2016-03-22 00:02:14 +0000

    Add game map Make better access to Rooms

--- src/qthgame/ZorkUL.cpp
+++ src/qthgame/ZorkUL.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "ZorkUL.h"
 
+#define ROOM(x) rooms[x-1]
+#define CORR(x) rooms[7+x]
+
 using namespace std;
 
 ZorkUL::ZorkUL() {
@@ -9,28 +12,56 @@ ZorkUL::ZorkUL() {
 }
 
 void ZorkUL::createGame() {
-    Room *a, *b, *c, *d;
+    Room *r1, *r2, *r3, *r4, *r5, *r6, *r7, *r8;
+    Room *c1, *c2, *c3, *c4, *c5, *c6, *c7;
 
-    a = new Room("cell", "You're in a cell. There is a door in front of you.");
-    b = new Room("cell room", "You are in the cell room. There is a desk on your side, and a door in front of you.");
-    c = new Room("corridor east", "You are in an end of a corridor. There is a plant on your right, with a door beside it, and the cell room on your left.");
-    d = new Room("corridor middle", "You are in the middle of the corridor. There is a door in your right and another on your left.");
+    r1 = new Room("cell", "You're in a cell. There is a door in front of you.");
+    r2 = new Room("cell room", "You are in the cell room. There is a desk on your side, and a door in front of you.");
+    r3 = new Room("", "");
+    r4 = new Room("", "");
+    r5 = new Room("", "");
+    r6 = new Room("", "");
+    r7 = new Room("", "");
+    r8 = new Room("", "");
+    c1 = new Room("corridor east", "You are in an end of a corridor. There is a plant on your right, with a door beside it, and the cell room on your left.");
+    c2 = new Room("corridor middle", "You are in the middle of the corridor. There is a door in your right and another on your left.");
+    c3 = new Room("", "");
+    c4 = new Room("", "");
+    c5 = new Room("", "");
+    c6 = new Room("", "");
+    c7 = new Room("", "");
 
-    rooms.push_back(a);
-    rooms.push_back(b);
-    rooms.push_back(c);
-    rooms.push_back(d);
+    rooms.push_back(r1);
+    rooms.push_back(r2);
+    rooms.push_back(r3);
+    rooms.push_back(r4);
+    rooms.push_back(r5);
+    rooms.push_back(r6);
+    rooms.push_back(r7);
+    rooms.push_back(r8);
+    rooms.push_back(c1);
+    rooms.push_back(c2);
+    rooms.push_back(c3);
+    rooms.push_back(c4);
+    rooms.push_back(c5);
+    rooms.push_back(c6);
+    rooms.push_back(c7);
 
-    Gateway *ab, *bc, *cd;
-    ab = new Gateway(0, a, b);
-    bc = new Gateway(1, b, c);
-    cd = new Gateway(2, c, d);
+    gateways.push_back(new Gateway(0, r1, r2));
+    gateways.push_back(new Gateway(1, r2, c1));
+    gateways.push_back(new Gateway(2, c1, c2));
+    gateways.push_back(new Gateway(3, c2, r3));
+    gateways.push_back(new Gateway(4, r3, r8));
+    gateways.push_back(new Gateway(5, c2, r4));
+    gateways.push_back(new Gateway(6, c2, c3));
+    gateways.push_back(new Gateway(7, c3, c4));
+    gateways.push_back(new Gateway(8, c4, r6));
+    gateways.push_back(new Gateway(9, c4, c5));
+    gateways.push_back(new Gateway(10, c5, c6));
+    gateways.push_back(new Gateway(11, c6, c7));
+    gateways.push_back(new Gateway(12, c6, r7));
 
-    gateways.push_back(ab);
-    gateways.push_back(bc);
-    gateways.push_back(cd);
-
-    currentRoom = a;
+    currentRoom = r1;
 }
 
 /**
@@ -88,17 +119,17 @@ string ZorkUL::getMessage() {
 
 vector<string> ZorkUL::getOptions() {
     vector<string> o;
-    if (currentRoom == rooms[0]) {
+    if (currentRoom == ROOM(1)) {
         o.push_back("Pass through door");
-    } else if (currentRoom == rooms[1]) {
+    } else if (currentRoom == ROOM(2)) {
         o.push_back("Go to the cell");
         o.push_back("Go through the other door");
-    } else if (currentRoom == rooms[2]) {
+    } else if (currentRoom == CORR(1)) {
         o.push_back("Check plant");
         o.push_back("Go back to the cell room");
         o.push_back("Go through the door on your right.");
         o.push_back("Continue in the corridor");
-    } else if (currentRoom == rooms[3]) {
+    } else if (currentRoom == CORR(2)) {
         o.push_back("Go to the door on your left");
         o.push_back("Go to the door on your right");
         o.push_back("Go back in the corridor");
@@ -110,17 +141,17 @@ vector<string> ZorkUL::getOptions() {
 }
 
 void ZorkUL::doAction(int choice) {
-    if (currentRoom == rooms[0]) {
+    if (currentRoom == ROOM(1)) {
         if (choice == 1) {changeRoom(gateways[0]);}
-    } else if (currentRoom == rooms[1]) {
+    } else if (currentRoom == ROOM(2)) {
         if (choice == 1) {changeRoom(gateways[0]);}
         else if (choice == 2) {changeRoom(gateways[1]);}
-    } else if (currentRoom == rooms[2]) {
+    } else if (currentRoom == CORR(1)) {
         if (choice == 1) {cout << "You checked the plant, UHUUUUL!" << endl;}
         else if (choice == 2) {changeRoom(gateways[1]);}
         else if (choice == 3) {cout << "Door is locked." << endl;}
         else if (choice == 4) {changeRoom(gateways[2]);}
-    } else if (currentRoom == rooms[3]) {
+    } else if (currentRoom == CORR(2)) {
         if (choice == 1) {cout << "Door is locked." << endl;}
         else if (choice == 2) {cout << "Door is locked." << endl;}
         else if (choice == 3) {changeRoom(gateways[2]);}
